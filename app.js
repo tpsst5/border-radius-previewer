3a8f64b20cd98e1c339db56dd2d5138cc6bf0916
Timothy Shea <timothyshea@system76-pc.localdomain>
2020-07-19 21:39:04 -0400

    Updated files

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-// Event listener
-document.getElementById('square-1').addEventListener('mousedown', changeLeft);
+const leftSelector = document.getElementById('square-1');
+const topSelector = document.getElementById('square-2');
+const rightSelector = document.getElementById('square-3');
+const bottomSelector = document.getElementById('square-4');
 
-function changeLeft() {
+let x = 0;
+let y = 0;
+let adjustingRadius = false;
 
+leftSelector.addEventListener('mousedown', e => {
+  y = e.offsetY;
+  adjustingRadius = true;
+});
+
+leftSelector.addEventListener('mousemove', e => {
+  if (adjustingRadius === true) {
+    changeRadius(y, e.offsetY);
+    y = 0;
+  }
+});
+
+window.addEventListener('mouseup', e => {
+  if (adjustingRadius === true) {
+    changeRadius(y, e.offsetY);
+    y = 0;
+    adjustingRadius = false;
+  }
+});
+
+
+function changeRadius(y, yOffSet) {
+  leftSelector.style.bottom = `-${yOffSet}px`;
+  console.log(y, yOffSet);
 }

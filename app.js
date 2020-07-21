d8705dab1dfb45b9a701a61cb8e81602ec27e7f2
Timothy Shea <timothyshea@system76-pc.localdomain>
2020-07-20 22:40:40 -0400

    Updated files

--- app.js
+++ app.js
@@ -2,33 +2,60 @@ const leftSelector = document.getElementById('square-1');
 const topSelector = document.getElementById('square-2');
 const rightSelector = document.getElementById('square-3');
 const bottomSelector = document.getElementById('square-4');
+const border = document.getElementById('border');
+const inputText = document.getElementById('border-input');
 
-let x = 0;
-let y = 0;
-let adjustingRadius = false;
+let borderRadius = ['100%', '0%', '100%', '0%', '/', '0%', '100%', '0%', '100%'];
 
-leftSelector.addEventListener('mousedown', e => {
-  y = e.offsetY;
-  adjustingRadius = true;
-});
 
-leftSelector.addEventListener('mousemove', e => {
-  if (adjustingRadius === true) {
-    changeRadius(y, e.offsetY);
-    y = 0;
+leftSelector.onmousedown = function (event) {
+  const sidePixelLength = 494 - 123;
+  let percentMoved = 0;
+  let borderRadiusString = null;
+  // centers the leftSelector at pageY coordinates
+  function moveAt(pageY) { //(pageX, pageY) {
+    // leftSelector.style.left = pageX - leftSelector.offsetWidth / 2 + 'px';
+    if (pageY >= 123 && pageY <= 494) {
+      leftSelector.style.top = pageY - 130 + 'px'; //pageY - leftSelector.offsetHeight / 2 + 'px';
+      percentMoved = ((pageY - 123) / sidePixelLength).toFixed(2);
+
+      if (percentMoved !== '0.00' || percentMoved !== '1.00') {
+        borderRadius[5] = `${percentMoved.substring(2)}%`;
+        borderRadius[8] = `${(100 - Number(percentMoved.substring(2)))}%`;
+        borderRadiusString = String(borderRadius
+          .join(',')
+          .replace(/,/g, ' ')
+          .split());
+        border.style.borderRadius = borderRadiusString;
+        inputText.value = borderRadiusString;
+      }
+    }
+    // At pageY === 493 & 494 it glitches back to 0 100 0 100  ATTENTION!!!
+    else if (pageY > 400) {
+      border.style.borderRadius = '100% 0% 100% 0% / 100% 100% 0% 0%';
+      inputText.value = '100% 0% 100% 0% / 100% 100% 0% 0%';
+    }
+    // console.log(borderRadiusString);
+    console.log(pageY);
   }
-});
 
-window.addEventListener('mouseup', e => {
-  if (adjustingRadius === true) {
-    changeRadius(y, e.offsetY);
-    y = 0;
-    adjustingRadius = false;
+  // move our absolutely positioned leftSelector under the pointer
+  moveAt(event.pageY); //(event.pageX, event.pageY);
+
+  function onMouseMove(event) {
+    moveAt(event.pageY); //(event.pageX, event.pageY);
   }
-});
 
+  // (2) move the leftSelector on mousemove
+  document.addEventListener('mousemove', onMouseMove);
 
-function changeRadius(y, yOffSet) {
-  leftSelector.style.bottom = `-${yOffSet}px`;
-  console.log(y, yOffSet);
-}
+  // (3) drop the leftSelector, remove unneeded handlers
+  window.onmouseup = function () {
+    document.removeEventListener('mousemove', onMouseMove);
+    window.onmouseup = null;
+  };
+};
+
+leftSelector.ondragstart = function () {
+  return false;
+};
